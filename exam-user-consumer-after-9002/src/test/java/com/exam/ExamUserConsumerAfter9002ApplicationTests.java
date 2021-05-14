6dd3c4540717f815e392a7eddfc1697d9c28ebff
yangtao <1678407878@qq.com>
2021-05-14 19:54:32 +0800

    前端 消费者9002 后端基本功能完成

---
+++ exam-user-consumer-after-9002/src/test/java/com/exam/ExamUserConsumerAfter9002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamUserConsumerAfter9002ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
